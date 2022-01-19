66b68f85aefe4a19a52e3760859502ae3e2b242c
alirezabashi98 <alirezabashi98@gmail.com>
2022-01-19 20:56:30 +0330

    Adjust the color of the theme and adjust the overall format of the project, for example, use the main project as the main view

--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.alireza.bashi.learn.jetpack.compose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.alireza.bashi.learn.jetpack.compose.ui.theme.LearnJetpackComposeTheme
 
@@ -24,6 +28,8 @@ class MainActivity : ComponentActivity() {
 //    uiMode = UI_MODE_NIGHT_YES
 //    name = "DefaultPreviewDark"
 @Preview(showBackground = true)
+@Preview(showBackground = true,showSystemUi = true,widthDp = 320,name = "DefaultPreviewLight")
+@Preview(showBackground = true,uiMode = UI_MODE_NIGHT_YES,showSystemUi = true, widthDp = 320,name = "DefaultPreviewDark")
 @Composable
 fun DefaultPreview() {
     LearnJetpackComposeTheme {
--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/ui/theme/Theme.kt
+++ app/src/main/java/com/alireza/bashi/learn/jetpack/compose/ui/theme/Theme.kt
@@ -7,15 +7,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = White,
+    primary = LightBlue,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color.White,
