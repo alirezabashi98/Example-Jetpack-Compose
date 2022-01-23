64a7d44a085e64886ab9d373a724598f4d8f8ebc
alirezabashi98 <alirezabashi98@gmail.com>
2022-01-23 21:29:48 +0330

    step3 User Image

--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
@@ -1,34 +1,37 @@
 package com.alireza.bashi.learn.jetpack.compose
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.alireza.bashi.learn.jetpack.compose.ui.theme.LearnJetpackComposeTheme
+import com.google.accompanist.coil.rememberCoilPainter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LearnJetpackComposeTheme {
-                // تنظیم تم شب و روز نیاز به زمین داریم که کمپوز ها روش بچینیم مثل سارفیس
-                Surface(color = MaterialTheme.colors.background) {
-                    DefaultPreview()
-                }
+                DefaultPreview()
             }
         }
     }
@@ -47,61 +50,88 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     LearnJetpackComposeTheme {
-        ButtonExample()
+        // تنظیم تم شب و روز نیاز به زمین داریم که کمپوز ها روش بچینیم مثل سارفیس
+        Surface(color = MaterialTheme.colors.background) {
+            ImageExample()
+        }
     }
 }
 
 @Composable
-fun ButtonExample() {
+fun ImageExample() {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+            .fillMaxSize()
+            .padding(8.dp)
+            .shadow(4.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        var saveStatus by remember { mutableStateOf("Hello World") }
-
-        Box(
+        // image bitmap
+        Text(
+            text = "Bitmap",
+            modifier = Modifier.padding(top = 16.dp),
+            fontSize = 15.sp
+        )
+        Image(
+            painter = painterResource(id = R.drawable.compose),
+            contentDescription = "Android",
             modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .background(Color.LightGray)
-                .padding(16.dp),
-            contentAlignment = Alignment.Center,
-        ) {
-            Text(
-                text = saveStatus,
-                fontSize = 30.sp,
-                modifier = Modifier.clickable { saveStatus = "Hello World" }
-            )
+                .width(300.dp)
+                .height(160.dp)
+                .padding(16.dp)
+                .clip(RoundedCornerShape(10))
+        )
+
+        //image vector
+//        Text(
+//            text = "Vector",
+//            modifier = Modifier.padding(top = 16.dp),
+//            fontSize = 15.sp
+//        )
+//        Image(
+//            painter = painterResource(id = R.drawable.vector),
+//            contentDescription = "Game",
+//            modifier = Modifier
+//                .width(300.dp)
+//                .height(200.dp)
+//                .padding(16.dp)
+//        )
+
+        //image Icon
+//        Text(
+//            text = "Icon",
+//            modifier = Modifier.padding(top = 16.dp),
+//            fontSize = 15.sp
+//        )
+//        Icon(
+//            imageVector = Icons.Outlined.Person,
+//            contentDescription = "User"
+//        )
+
+        //image accompanist-coil
+        Text(
+            text = "accompanist coil",
+            modifier = Modifier.padding(top = 16.dp),
+            fontSize = 15.sp
+        )
+
+        var linkImage by remember { mutableStateOf("") }
+        val painter = rememberCoilPainter(request = linkImage)
+        Image(
+            painter = painter,
+            contentDescription = "Link",
+            modifier = Modifier
+                .width(300.dp)
+                .height(200.dp)
+                .padding(16.dp)
+                .background(Color.Black)
+                .clip(RoundedCornerShape(10))
+        )
+        Button(onClick = {
+            linkImage = "https://static.cdn.asset.aparat.com/profile-photo/6378539-m.jpg"
+        }) {
+            Text(text = "Load Image")
         }
 
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Button(
-            onClick = { saveStatus = "Clicked Button 1" },
-            modifier = Modifier.padding(16.dp),
-            border =  BorderStroke(2.dp, Color.White)
-        ) {
-            Text(text = stringResource(id = R.string.Example_Button_1))
-        }
-
-        Button(
-            onClick = { saveStatus = "Clicked Button 2" },
-            modifier = Modifier.padding(16.dp),
-            border =  BorderStroke(2.dp, Color.Black)
-        ) {
-            Text(text = stringResource(id = R.string.Example_Button_2))
-        }
-
-        Button(
-            onClick = { saveStatus = "Clicked Button 3" },
-            modifier = Modifier.padding(16.dp),
-            border =  BorderStroke(2.dp, Color.Yellow),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Unspecified)
-        ) {
-            Text(text = stringResource(id = R.string.Example_Button_3))
-        }
     }
 }
