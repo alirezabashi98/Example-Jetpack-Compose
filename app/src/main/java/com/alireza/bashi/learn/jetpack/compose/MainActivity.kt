f807506faca17ea004778a8894e9f4557fff5991
alirezabashi98 <alirezabashi98@gmail.com>
2022-01-27 12:13:28 +0330

    step7 Scaffold

--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
@@ -4,18 +4,22 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.alireza.bashi.learn.jetpack.compose.ui.theme.LearnJetpackComposeTheme
-import org.intellij.lang.annotations.JdkConstants
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,80 +47,54 @@ fun DefaultPreview() {
     LearnJetpackComposeTheme {
         // تنظیم تم شب و روز نیاز به زمین داریم که کمپوز ها روش بچینیم مثل سارفیس
         Surface(color = MaterialTheme.colors.background) {
-            Box(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Box(
-                    modifier = Modifier.align(Alignment.TopCenter)
-                ) {
-                    MyTopAppBar()
-                }
-                Box(
-                    modifier = Modifier.align(Alignment.BottomCenter)
-                ) {
-                    MyBottomAppBar()
-                }
-            }
+            ScaffoldDemo()
         }
     }
 }
 
 @Composable
-fun MyTopAppBar() {
-    TopAppBar(
-        title = {
-            Text(text = stringResource(id = R.string.app_name))
-        },
-        navigationIcon = {
-            IconButton(onClick = { }) {
-                Icon(
-                    imageVector = Icons.Default.Menu,
-                    contentDescription = "Menu"
-                )
-            }
-        },
-        actions = {
-            IconButton(onClick = { }) {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = "Search"
-                )
-            }
-            IconButton(onClick = { }) {
-                Icon(
-                    imageVector = Icons.Default.Favorite,
-                    contentDescription = "Favorite"
-                )
-            }
-        },
-    )
-}
+fun ScaffoldDemo() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    var count by remember { mutableStateOf(0) }
 
-@Composable
-fun MyBottomAppBar() {
-    BottomAppBar() {
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Menu")
+                    }
+                }
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            ExtendedFloatingActionButton(text = { Text(text = "Fab") }, onClick = { count++ })
+        },
+        drawerContent = {
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Favorite")
+            Text(text = "ok")
+        }
+    ) {padding ->
+        Box(
             modifier = Modifier.fillMaxSize()
         ) {
-            IconButton(onClick = { }) {
-                Icon(
-                    imageVector = Icons.Default.Notifications,
-                    contentDescription = "Notifications"
+            Text(
+                text = "Count : $count",
+                modifier = Modifier.align(Alignment.Center)
                 )
-            }
-            IconButton(onClick = { }) {
-                Icon(
-                    imageVector = Icons.Default.Home,
-                    contentDescription = "Home"
-                )
-            }
-            IconButton(onClick = { }) {
-                Icon(
-                    imageVector = Icons.Default.Settings,
-                    contentDescription = "Settings"
-                )
-            }
         }
     }
 }
