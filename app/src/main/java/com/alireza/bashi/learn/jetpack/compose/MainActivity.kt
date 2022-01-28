74dfbd08873371b173145f55391ebd43c61fea8d
alirezabashi98 <alirezabashi98@gmail.com>
2022-01-28 16:25:59 +0330

    step9 animation

--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
@@ -4,22 +4,28 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alireza.bashi.learn.jetpack.compose.ui.theme.LearnJetpackComposeTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,56 +51,42 @@ fun DefaultPreview() {
     LearnJetpackComposeTheme {
         // تنظیم تم شب و روز نیاز به زمین داریم که کمپوز ها روش بچینیم مثل سارفیس
         Surface(color = MaterialTheme.colors.background) {
-            ScaffoldDemo()
+            AnimationExample()
         }
     }
 }
 
 @Composable
-fun ScaffoldDemo() {
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
-    var count by remember { mutableStateOf(0) }
+fun AnimationExample() {
+    val (animate, onAnimateChange) = remember { mutableStateOf(false) }
+    val cornerSize by animateFloatAsState(targetValue = if (animate) 500f else 10f)
+    val imageHeight by animateDpAsState(
+        targetValue = if (animate) 300.dp else 200.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessHigh
+        )
+    )
 
-    Scaffold(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        scaffoldState = scaffoldState,
-        topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.app_name)) },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        scope.launch {
-                            scaffoldState.drawerState.open()
-                        }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = "Menu"
-                        )
-                    }
-                }
-            )
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        floatingActionButton = {
-            ExtendedFloatingActionButton(text = { Text(text = "Fab") }, onClick = { count++ })
-        },
-        drawerContent = {
-            Icon(
-                imageVector = Icons.Default.Favorite,
-                contentDescription = "Favorite"
-            )
-            Text(text = "ok")
-        }
-    ) { padding ->
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = "Count : $count",
-                modifier = Modifier.align(Alignment.Center)
-            )
-        }
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Switch(
+            checked = animate,
+            onCheckedChange = onAnimateChange,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.alireza),
+            contentDescription = "may Image",
+            modifier = Modifier
+                .clip(RoundedCornerShape(cornerSize))
+                .fillMaxWidth()
+                .height(imageHeight)
+                .padding(8.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
