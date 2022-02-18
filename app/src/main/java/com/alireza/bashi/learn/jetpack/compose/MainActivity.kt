56df48951825ef150d960f2378ca3510b677be68
AlirezaBashi98 <alirezabashi98@gmail.com>
2022-02-18 08:54:14 +0330

    add card image

--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
@@ -4,15 +4,25 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.runtime.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.unit.dp
 import com.alireza.bashi.learn.jetpack.compose.ui.theme.LearnJetpackComposeTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,58 +48,49 @@ fun DefaultPreview() {
     LearnJetpackComposeTheme {
         // تنظیم تم شب و روز نیاز به زمین داریم که کمپوز ها روش بچینیم مثل سارفیس
         Surface(color = MaterialTheme.colors.background) {
-            SnackBarExample()
+            ExampleCardImage()
         }
     }
 }
 
 @Composable
-fun SnackBarExample() {
-    val snackBarState = SnackbarHostState()
-    val scaffoldState = rememberScaffoldState(snackbarHostState = snackBarState)
-    val scope = rememberCoroutineScope()
-    var showDialog by remember { mutableStateOf(false) }
-    var bgColor by remember { mutableStateOf(Color.Blue) }
-
-    Scaffold(
-        scaffoldState = scaffoldState,
-        backgroundColor = bgColor,
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text(text = "Show SnackBar") },
-                onClick = {
-                    scope.launch {
-                        val result = scaffoldState.snackbarHostState.showSnackbar("Hello Jetpack Compose", actionLabel = "ok")
-                        if (result == SnackbarResult.ActionPerformed) showDialog = true
-                    }
-                },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Default.Notifications,
-                        contentDescription = "Notifications"
-                    )
-                }
-            )
-        }
+fun ExampleCardImage() {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .width(150.dp)
+            .height(200.dp)
+            .clip(RoundedCornerShape(16.dp))
     ) {
-
-        if (showDialog){
-            AlertDialog(
-                onDismissRequest = { /*TODO*/ },
-                confirmButton = { Button(onClick = {
-                    bgColor = Color.Yellow
-                    showDialog = false
-                }) {
-                    Text(text = "ok")
-                }},
-                dismissButton = { Button(onClick = { showDialog = false }) {
-                    Text(text = "cancel")
-                }},
-                title = {Text(text = "AlertDialog Example") },
-                text = { Text(text = "AlertDialog text") },
-                properties = DialogProperties(dismissOnBackPress = false,dismissOnClickOutside = false)
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.alireza),
+                contentDescription = "alirezabashi98",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.BottomCenter)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 180f
+                        )
+                    )
+            )
+            Text(
+                text = "AlirezaBashi98",
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.BottomCenter)
             )
         }
-
     }
 }
