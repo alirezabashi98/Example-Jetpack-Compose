e3781e0a1adad706350d38d87a1b43ded061c49f
AlirezaBashi98 <alirezabashi98@gmail.com>
2022-03-12 12:45:04 +0330

    learn Constraint Layout In Jetpack Cojmpose

--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
@@ -4,24 +4,18 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import com.alireza.bashi.learn.jetpack.compose.ui.theme.LearnJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,49 +42,55 @@ fun DefaultPreview() {
     LearnJetpackComposeTheme {
         // تنظیم تم شب و روز نیاز به زمین داریم که کمپوز ها روش بچینیم مثل سارفیس
         Surface(color = MaterialTheme.colors.background) {
-            ExampleCardImage()
+            ExampleConstraintLayout2()
         }
     }
 }
 
 @Composable
-fun ExampleCardImage() {
-    Card(
-        modifier = Modifier
-            .padding(16.dp)
-            .width(150.dp)
-            .height(200.dp)
-            .clip(RoundedCornerShape(16.dp))
-    ) {
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.alireza),
-                contentDescription = "alirezabashi98",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .align(Alignment.BottomCenter)
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 180f
-                        )
-                    )
-            )
-            Text(
-                text = "AlirezaBashi98",
-                modifier = Modifier
-                    .padding(10.dp)
-                    .align(Alignment.BottomCenter)
-            )
+fun ExampleConstraintLayout() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (button, text) = createRefs()
+
+        val guideLineStart = createGuidelineFromStart(0.5f)
+
+        Button(onClick = { /*TODO*/ }, Modifier.constrainAs(button) {
+            top.linkTo(parent.top, margin = 16.dp)
+            start.linkTo(parent.start, margin = 16.dp)
+        }) {
+            Text(text = "Button")
         }
+        Text(text = "text", Modifier.constrainAs(text) {
+            top.linkTo(button.bottom)
+            start.linkTo(guideLineStart)
+        })
+    }
+}
+
+@Composable
+fun ExampleConstraintLayout2() {
+    val constrains = ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        val guidLineStart = createGuidelineFromStart(0.5f)
+
+        constrain(button) {
+            top.linkTo(parent.top, margin = 16.dp)
+            start.linkTo(parent.start, margin = 16.dp)
+        }
+
+        constrain(text) {
+            top.linkTo(button.bottom)
+            start.linkTo(guidLineStart)
+        }
+
+    }
+
+    ConstraintLayout(constrains,Modifier.fillMaxSize()) {
+        Button(onClick = { /*TODO*/ }, Modifier.layoutId("button")) {
+            Text(text = "button 2")
+        }
+        Text(text = "text 2", Modifier.layoutId("text"))
     }
 }
