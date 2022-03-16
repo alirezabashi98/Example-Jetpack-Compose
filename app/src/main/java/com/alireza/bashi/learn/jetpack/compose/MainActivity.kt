92c858e4be3393002788831c19623a16429d3c36
AlirezaBashi98 <alirezabashi98@gmail.com>
2022-03-16 13:16:01 +0330

    make example login for compose

--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/learn/jetpack/compose/MainActivity.kt
@@ -6,19 +6,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Login
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alireza.bashi.learn.jetpack.compose.ui.theme.LearnJetpackComposeTheme
@@ -36,24 +37,24 @@ class MainActivity : ComponentActivity() {
 //    showSystemUi = true
 //    uiMode = UI_MODE_NIGHT_YES
 //    name = "DefaultPreviewDark"
+//@Preview(
+//    showBackground = true,
+//    uiMode = UI_MODE_NIGHT_YES,
+//    name = "DefaultPreviewDark"
+//)
 @Preview(showBackground = true)
-@Preview(
-    showBackground = true,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
 @Composable
 fun DefaultPreview() {
     LearnJetpackComposeTheme {
         // تنظیم تم شب و روز نیاز به زمین داریم که کمپوز ها روش بچینیم مثل سارفیس
         Surface(color = MaterialTheme.colors.background) {
-            ExampleAnimations()
+            ExampleLogin()
         }
     }
 }
 
 @Composable
-fun ExampleAnimations() {
+fun ExampleLogin() {
     var sizeState by remember { mutableStateOf(200.dp) }
     val sizeAnim by animateDpAsState(
         targetValue = sizeState,
@@ -65,21 +66,52 @@ fun ExampleAnimations() {
     val infiniteTransition = rememberInfiniteTransition()
     val color by infiniteTransition.animateColor(
         initialValue = Color.Blue,
-        targetValue = Color.Red,
+        targetValue = Color.Magenta,
         animationSpec = infiniteRepeatable(
             tween(durationMillis = 2000),
             repeatMode = RepeatMode.Reverse
         )
     )
 
-    Box(
+    Column(
         modifier = Modifier
-            .background(color)
-            .size(sizeAnim),
-        contentAlignment = Alignment.Center
+            .fillMaxSize()
+            .background(color),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Button(onClick = { sizeState += 50.dp }) {
-            Text(text = "Example")
+
+        Row {
+            Text(text = "خوش آمدید",color = Color.White)
+            Spacer(modifier = Modifier.width(5.dp))
+            Icon(imageVector = Icons.Default.Login , contentDescription = "Login",tint = Color.White)
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Image(
+            modifier = Modifier.size(200.dp),
+            painter = painterResource(id = R.drawable.login),
+            contentDescription = "Page Login")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedButton(
+            modifier = Modifier
+                .width(200.dp)
+                .height(40.dp),
+            onClick = { /*TODO*/ }) {
+            Text(text = "ورود به حساب")
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(
+            modifier = Modifier
+                .width(200.dp)
+                .height(40.dp),
+            onClick = { /*TODO*/ }) {
+            Text(text = "ثبت نام")
         }
     }
 }
