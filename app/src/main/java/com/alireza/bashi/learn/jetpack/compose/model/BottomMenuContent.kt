db055e7957fbfba7baed043bef871a9823d16591
AlirezaBashi98 <alirezabashi98@gmail.com>
2022-03-30 20:30:56 +0430

    remove ui branch back

--- app/src/main/java/com/alireza/bashi/learn/jetpack/compose/model/BottomMenuContent.kt
+++
@@ -1,8 +0,0 @@
-package com.alireza.bashi.learn.jetpack.compose.model
-
-import androidx.annotation.DrawableRes
-
-data class BottomMenuContent(
-    val title : String,
-    @DrawableRes val iconId : Int,
-)
